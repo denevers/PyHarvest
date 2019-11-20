5c91117a20a2e4e2fb9b97e334fbd3dbbe49271e
Joost van Ulden <joost.vanulden@canada.ca>
2019-11-20 17:00:43 +0100

    Harvest cross relations only
    Note: this example uses the USGS node as the master.

--- src/crawler.py
+++ src/crawler.py
@@ -9,8 +9,10 @@ from rdflib import URIRef, Graph, plugin
 from rdflib.namespace import DCTERMS, RDF, RDFS
 from rdflib.serializer import Serializer
 
-MASTER_NODE = "https://geoconnex.ca/id/LOD_Node/CAN_Hydro_LOD_Node"
-CONNECTED_PREDICATE = URIRef("https://geoconnex.ca/id/properties/connectedTo")
+# MASTER_NODE = "https://geoconnex.ca/id/LOD_Node/CAN_Hydro_LOD_Node"
+MASTER_NODE = "https://cida-test.er.usgs.gov/chyld-pilot/info/LOD_Node/US_Hydro_LOD_Node"
+# CONNECTED_PREDICATE = URIRef("https://geoconnex.ca/id/properties/connectedTo")
+CONNECTED_PREDICATE = URIRef("https://geoconnex.ca/id/connectedTo")
 
 # If you are not sure what format your file will be, 
 # you can use rdflib.util.guess_format() which will 
@@ -31,14 +33,20 @@ def main():
 
     connected_nodes = harvest_nodes(g)
 
+    # Temporary hack until GSIP is updated with the correct
+    # node URL https://cida-test.er.usgs.gov/chyld-pilot/info/LOD_Node/US_Hydro_LOD_Node
+
+    # connected_nodes = ['https://cida-test.er.usgs.gov/chyld-pilot/info/LOD_Node/US_Hydro_LOD_Node']
+
     print("\n--- printing connected linked open data nodes ---\n")
     for i, node in enumerate(connected_nodes, start=1):
         print("{}: {}\n".format(i, node))
 
         g = rdflib.Graph()
         try:
-            g.parse(node)
+            r = g.parse(node)
         except:
+            print(r)
             print("An error occurred whilst parsing the target resource!\n")
             continue
 
@@ -61,24 +69,30 @@ def harvest_triples(graph):
     # for s, p, o in graph:
     #     print((s, p, o))
 
-    print("\n--- printing N3 triples ---\n")
-    n = graph.serialize(format="n3")
-    print(n.decode())
+    # print("\n--- printing N3 triples ---\n")
+    # n = graph.serialize(format="n3")
+    # print(n.decode())
 
-    for s in graph.subjects(predicate=URIRef('http://purl.org/dc/terms/format'), object=rdflib.term.Literal('application/rdf+xml')):
-        g = rdflib.Graph()
-        # set to XML because GitHub is sending as text\plain
-        g.parse(s, format='xml')
+    # for s in graph.subjects(predicate=URIRef('http://schema.org/subjectOf'), object=rdflib.term.Literal('application/rdf+xml')):
+    for s in graph.objects(predicate=URIRef('http://schema.org/subjectOf')):
+
+        if (s, RDF.type, URIRef('https://opengeospatial.github.io/SELFIE/DataNode_FeatureLinkSet')) in graph:
         
-        harvest_feature_relations(g)
+            g = rdflib.Graph()
+
+            g.parse(s)
+        
+            harvest_feature_relations(g)
+
+            break
 
 def harvest_feature_relations(graph):
     """harvest feature relations and store them"""
     
     n = graph.serialize(format="n3")
 
-    # print("\n--- printing N3 triples ---\n")
-    # print(n.decode())
+    print("\n--- printing N3 triples ---\n")
+    print(n.decode())
     
     # Write triples to file in n3 format
     f = open('data.n3', 'w')
@@ -86,16 +100,16 @@ def harvest_feature_relations(graph):
     f.close()
 
     # Post triple to triple store
-    try:
-        url = 'http://127.0.0.1:9999/blazegraph/sparql'
-        payload = open('data.n3')
-        headers = {'content-type': 'text/n3', 'Accept-Charset': 'UTF-8'}
-        r = requests.post(url, data=payload, headers=headers)
+    # try:
+    #     url = 'http://127.0.0.1:9999/blazegraph/sparql'
+    #     payload = open('data.n3')
+    #     headers = {'content-type': 'text/n3', 'Accept-Charset': 'UTF-8'}
+    #     r = requests.post(url, data=payload, headers=headers)
 
-        print('\n' + r.content.decode() + '\n')
-    except requests.exceptions.RequestException as e:  # This is the correct syntax
-        print(e)
-        sys.exit(1)
+    #     print('\n' + r.content.decode() + '\n')
+    # except requests.exceptions.RequestException as e:  # This is the correct syntax
+    #     print(e)
+    #     sys.exit(1)
 
 def validate_triples(triple):
     """validate triple"""
